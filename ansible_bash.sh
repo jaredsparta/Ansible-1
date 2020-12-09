d08c14f08ae2923e141cde20bc7190099f98a37a
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-12-09 14:11:39 +0000

    python and pip installs

--- ansible_bash.sh
+++ ansible_bash.sh
@@ -4,3 +4,13 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+# Install python and dependencies
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo pip install --upgrade pip -y
+
+# Installing
+pip3 install awscli
+sudo pip install boto -y
+sudo pip install boto3 -y
