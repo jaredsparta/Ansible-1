c275121ce3ccc9de64ceebd6565a3d13aca0a163
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-12-12 13:38:51 +0000

    one line

--- install-ansible.sh
+++ install-ansible.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt install software-properties-common -yes
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+sudo mv /etc/ansible/hosts /etc/ansible/hosts.backup
